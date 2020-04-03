d922493077eb18e68f7d5a419736a8ddef8a1823
mghasemi <mehdi.ghasemi@gmail.com>
2020-04-02 19:58:00 -0600

    Bayesian Ridge compatible

--- GeneralRegression/GeneralRegression.py
+++ GeneralRegression/GeneralRegression.py
@@ -6,6 +6,8 @@ class GenericRegressor(BaseEstimator, RegressorMixin):
         from sklearn.linear_model import LinearRegression
         self.funcs = funcs
         self.kw_ar = kwargs
+        self.std = False
+        self.standard_deviation = []
         if regressor is None:
             self.regressor = LinearRegression
         else:
@@ -15,19 +17,34 @@ class GenericRegressor(BaseEstimator, RegressorMixin):
     def fit(self, X, y):
         from sklearn.pipeline import make_pipeline
         from sklearn.preprocessing import FunctionTransformer
-        self.model = make_pipeline(FunctionTransformer(self.funcs, kw_args=self.kw_ar, validate=False),
-                                   self.regressor())
+        from sklearn.linear_model import BayesianRidge
+        if self.regressor is BayesianRidge:
+            self.std = True
+            regressor = self.regressor(compute_score=True)
+        else:
+            regressor = self.regressor()
+        self.model = make_pipeline(  # MinMaxScaler((-1, 1)),
+            FunctionTransformer(self.funcs, kw_args=self.kw_ar, validate=False),
+            regressor)
         self.model.fit(X, y)
         return self
 
     def predict(self, X):
-        return self.model.predict(X)
+        if self.std:
+            pred, std = self.model.predict(X, return_std=True)
+        else:
+            pred = self.model.predict(X)
+            std = None
+        print(std)
+        self.standard_deviation = std
+        return pred
 
 
 ###########################################################
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import BayesianRidge
+
 
 def f(x):
     return x * np.sin(x) - np.sqrt(x) * np.cos(2 * x) + x ** 2
@@ -54,23 +71,31 @@ def fourier(X, n=1, l=1.):
 x_plot = np.linspace(0, 10, 100)
 # generate points and keep a subset of them
 x = np.linspace(0, 10, 100)
-rng = np.random.RandomState(0)
+rng = np.random  # .RandomState(0)
+size = 10
 rng.shuffle(x)
-x = np.sort(x[:10])
-y = f(x)
+x = np.sort(x[:size])
+y = f(x) + np.random.normal(scale=3., size=size)
 # create matrix versions of these arrays
 X = x[:, np.newaxis]
 X_plot = x_plot[:, np.newaxis]
-colors = ['teal', 'yellowgreen', 'gold', 'salmon', 'purple', 'orange', 'green', 'red']
+colors = ['yellowgreen', 'gold', 'salmon', 'purple', 'teal', 'orange', 'green', 'red']
 lw = 2
 plt.plot(x_plot, f(x_plot), color='cornflowerblue', linewidth=lw, label="ground truth")
-plt.scatter(x, y, color='navy', s=30, marker='o', label="training points")
+plt.scatter(x, y, color='navy', s=10, marker='o', label="training points")
 
-for count, degree in enumerate([7, 8, 9, 10]):
-    model = GenericRegressor(fourier, regressor=Ridge, **dict(n=degree, l=3))
+for count, degree in enumerate([7, 10]):
+    model = GenericRegressor(fourier, regressor=BayesianRidge, **dict(n=degree, l=3))
     model.fit(X, y)
     y_plot = model.predict(X_plot)
     plt.plot(x_plot, y_plot, color=colors[count], linewidth=lw, label="degree %d" % degree)
+    plt.fill_between(x_plot,
+                     y_plot - 1.96 * model.standard_deviation,# / np.sqrt(size),
+                     y_plot + 1.96 * model.standard_deviation,# / np.sqrt(size),
+                     color=colors[count],
+                     alpha=0.1)
 plt.legend(loc=2)
 
+print(np.exp(x))
+print(model.predict([[15]]), model.standard_deviation)
 plt.show()
